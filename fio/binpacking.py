b667d843e73dbdaf87ff8d9b762e60f95c04e45f
time <time@nixos>
2022-12-27 15:02:06 +0100

    typo

--- fio/binpacking.py
+++ fio/binpacking.py
@@ -22,47 +22,48 @@ import arith
 # %%
 
 def _optimize(inputs, outputs):
-    m = mip.Model()
-    m.verbose = 0
+  m = mip.Model()
+  m.verbose = 0
 
-    assert sum(inputs) == sum(outputs)
+  assert sum(inputs) == sum(outputs)
 
-    x = {
-      (i, j): m.add_var(f"flow_{i, j}", lb=0, ub=1)
-      for i, _ in enumerate(inputs)
-      for j, _ in enumerate(outputs)
-    }
-    y = {
-      (i, j): m.add_var(
-        f"flow_bool_{i, j}",
-        lb=0,
-        ub=1,
-        var_type=mip.BINARY,
-        obj=10000
-      )
-      for i, _ in enumerate(inputs)
-      for j, _ in enumerate(outputs)
-    }
+  x = {
+    (i, j): m.add_var(f"flow_{i, j}", lb=0, ub=1)
+    for i, _ in enumerate(inputs)
+    for j, _ in enumerate(outputs)
+  }
+  y = {
+    (i, j): m.add_var(
+      f"flow_bool_{i, j}",
+      lb=0,
+      ub=1,
+      var_type=mip.BINARY,
+      obj=10000
+    )
+    for i, _ in enumerate(inputs)
+    for j, _ in enumerate(outputs)
+  }
 
-    for i, _ in enumerate(inputs):
-        for j, _ in enumerate(outputs):
-            m.add_constr(x[i, j] <= y[i, j]*1)
+  for i, _ in enumerate(inputs):
+      for j, _ in enumerate(outputs):
+          m.add_constr(x[i, j] <= y[i, j]*1)
 
-    for i, v in enumerate(inputs):
-        m.add_constr(mip.quicksum(x[i, j] for j, _ in enumerate(outputs)) == v)
+  for i, v in enumerate(inputs):
+      m.add_constr(mip.quicksum(x[i, j] for j, _ in enumerate(outputs)) == v)
 
-    for j, v in enumerate(outputs):
-        m.add_constr(mip.quicksum(x[i, j] for i, _ in enumerate(inputs)) == v)
+  for j, v in enumerate(outputs):
+      m.add_constr(mip.quicksum(x[i, j] for i, _ in enumerate(inputs)) == v)
 
-    m.optimize(max_seconds=1)
+  m.optimize(max_seconds=1)
 
-    x = {k: arith.float_to_frac(v.x) for k, v in x.items() if arith.float_to_frac(v.x) > 0}
-    y = {k: arith.float_to_frac(v.x) for k, v in y.items()}
+  x = {k: arith.float_to_frac(v.x) for k, v in x.items() if arith.float_to_frac(v.x) > 0}
+  y = {k: arith.float_to_frac(v.x) for k, v in y.items()}
 
-    return {"x": x, "y": y}
+  return {"x": x, "y": y}
 
-
+# %%
 
 def optimize(inputs, outputs):
-    return _optimize(tuple(inputs), tuple(outputs))
+
+  return _optimize(tuple(inputs), tuple(outputs))
 
