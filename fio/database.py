763bc0ae92d282090dbb4499877f5899e4eaa208
time <time@nixos>
2022-12-27 11:25:50 +0100

    database using jq

--- fio/database.py
+++ fio/database.py
@@ -1,10 +1,24 @@
 import jq
+import json
+from pathlib import Path
+
+class bunch(dict):
+  def __getattribute__(self, name: str):
+    try:
+      return super().__getattribute__(name)
+    except AttributeError:
+      return self[name]
+
+class table(bunch):
+  def find(self, cmd):
+    return jq.compile(cmd).input(self).all()
+
+  def query(self, cmd):
+    return jq.compile(cmd).input(self).first()
 
 
-class Collection():
-    def __init__(self, data):
-        self.data = data
-
-    def find(self, query):
-        return jq.compile(query).input(self.data).all()
-
+def load_db(path):
+  db = bunch()
+  for f in Path(path).glob("*.json"):
+    db[f.stem.replace("-", "_")] = table(json.load(open(f)))
+  return db
